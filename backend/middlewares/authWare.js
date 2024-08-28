6cd68aefa49d6116af0712bea8d08dd803ff9344
alanjames00 <alanjames6282@gmail.com>
2024-08-29 01:32:37 +0530

    add auth pass

--- backend/middlewares/authWare.js
+++ backend/middlewares/authWare.js
@@ -2,7 +2,14 @@ const JwtHelper = require("../utils/jwtHelper");
 
 const jwtHelper = new JwtHelper();
 
+const authPassThrough = process.env.AUTH_PASS_THROUGH;
+console.log(authPassThrough);
+
 async function authWare(req, res, next) {
+	if (authPassThrough) {
+		req.user = "public";
+		return next();
+	}
 	const token = req.headers["x-auth-token"];
 	if (!token) {
 		return res.status(401).json({
